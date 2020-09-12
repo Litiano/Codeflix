74c263948a45a3989150c09ea6eaf07aa6c3445c
Litiano Moura <litiano.moura@yahoo.com.br>
2020-09-12 19:36:42 -0300

    Added Category and Genre tests.

---
+++ tests/Unit/Models/GenreTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Genre;
+use App\Models\UuidModel;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use PHPUnit\Framework\TestCase;
+
+class GenreTest extends TestCase
+{
+    private Genre $genre;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->genre = new Genre();
+    }
+
+    public function testUuid()
+    {
+        $this->assertTrue($this->genre instanceof UuidModel);
+        $this->assertFalse($this->genre->incrementing);
+        $this->assertEquals('string', $this->genre->getKeyType());
+    }
+
+    public function testFillableAttributes()
+    {
+        $this->assertEqualsCanonicalizing(['name', 'is_active'], $this->genre->getFillable());
+    }
+
+    public function testDates()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $this->assertEqualsCanonicalizing($dates, $this->genre->getDates());
+        $this->assertCount(count($dates), $this->genre->getDates());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [
+            SoftDeletes::class
+        ];
+        $this->assertEquals($traits, array_keys(class_uses(Genre::class)));
+    }
+
+    public function testCasts()
+    {
+        $this->assertEqualsCanonicalizing(['is_active' => 'bool'], $this->genre->getCasts());
+    }
+}
