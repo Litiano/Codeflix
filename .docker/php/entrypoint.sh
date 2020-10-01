fde55930dee1c6e39687ac5dfb585eb4361a333d
Litiano Moura <litiano.moura@yahoo.com.br>
2020-10-01 18:38:38 -0300

    Correção desafio.

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -2,6 +2,15 @@
 
 chown -R www-data:www-data .
 composer install
+
+if test -f ".env"; then
+    cp .env.example .env
+fi
+
+if test -f ".env.testing"; then
+    cp .env.testing.example .env.testing
+fi
+
 php artisan key:generate
 php artisan migrate
 
