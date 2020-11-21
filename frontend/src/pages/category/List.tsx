127d9d352228cda436596f73282d940c46a93f94
Litiano Moura <litiano.moura@yahoo.com.br>
2020-11-20 21:13:30 -0300

    Adicionado componentes: Nvbar, Breadcrumbs e Page.

---
+++ frontend/src/pages/category/List.tsx
@@ -0,0 +1,11 @@
+// @flow
+import * as React from 'react';
+import {Page} from "../../components/Page";
+
+const List = () => {
+    return (
+        <Page title={'Listagem categorias'}/>
+    );
+};
+
+export default List;
