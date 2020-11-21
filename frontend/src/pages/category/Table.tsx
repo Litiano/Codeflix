a9bd82a60aed6234d43a135355827b61ff9f20db
Litiano Moura <litiano.moura@yahoo.com.br>
2020-11-21 09:40:46 -0300

    Listagem de categorias.

---
+++ frontend/src/pages/category/Table.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import MUIDataTable, {MUIDataTableColumn} from "mui-datatables";
+import {useEffect, useState} from "react";
+import {httpVideo} from "../../utils/http";
+import {Chip} from "@material-ui/core";
+import {parseISO, format} from 'date-fns';
+
+const columnsDefinition: MUIDataTableColumn[] = [
+    {
+        name: 'name',
+        label: 'Nome',
+    },
+    {
+        name: 'is_active',
+        label: 'Ativo',
+        options: {
+            customBodyRender(value, tableMeta, updateValue): JSX.Element {
+                return value ? <Chip label={'Sim'} color={'primary'}/> : <Chip label={'Não'} color={'secondary'}/>
+            }
+        }
+    },
+    {
+        name: 'created_at',
+        label: 'Criado em',
+        options: {
+            customBodyRender(value, tableMeta, updateValue): JSX.Element {
+                return <span>{format(parseISO(value), 'dd/MM/yyyy')}</span>
+            }
+        }
+    },
+];
+
+type Props = {
+
+};
+const Table = (props: Props) => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        httpVideo.get('categories').then(response => {
+            setData(response.data.data);
+        })
+    }, []);
+
+    return (
+        <div>
+            <MUIDataTable columns={columnsDefinition} title='Listagem de categorias' data={data}/>
+        </div>
+    );
+};
+
+export default Table;
